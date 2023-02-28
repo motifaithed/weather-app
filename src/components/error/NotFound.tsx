a8dcdd15b4a6366d99b9d599e4c007bfdd952b08
Ronel Andaya <ronel.andaya@gmail.com>
2023-03-01 01:38:20 +0800

    change 404 text color to white

--- src/components/error/NotFound.tsx
+++ src/components/error/NotFound.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
     return <ul className="flex-col">
         <li><img src="../../../error-404.png" alt="" /></li>
-        <li className="font-semibold text-md text-center  text-normalGray mb-3">
+        <li className="font-semibold text-md text-center  text-white mb-3">
         We could not find what you are looking for. Please check the city spelling and search again.
       </li>
     </ul>
